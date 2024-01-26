f6315d518adffcd71d4c14fdb6c2bc4f2323a369
IsmaelBuere <7674@holbertonstudents.com>
2024-01-26 09:25:51 -0800

    task-4

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -2,15 +2,19 @@
 import marshal
 
 def print_names_from_pyc(pyc_file):
-    with open(pyc_file, 'rb') as file:
-        file.read(8)
-        
-        code_object = marshal.load(file)
-        names = [name for name in dir(code_object) if not name.startswith('__')]
+    try:
+        with open(pyc_file, 'rb') as file:
+            file.read(8)
+            
+            code_object = marshal.load(file)
+            names = [name for name in dir(code_object) if not name.startswith('__')]
 
-        for name in sorted(names):
-            print(name)
+            for name in sorted(names):
+                print(name)
+    except ValueError as e:
+        print(f"Error reading {pyc_file}: {e}")
 
 if __name__ == "__main__":
     pyc_file_path = 'hidden_4.pyc'
     print_names_from_pyc(pyc_file_path)
+
