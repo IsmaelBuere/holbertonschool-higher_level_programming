6a88191be1babe8250742f9745863aed29b65c32
IsmaelBuere <7674@holbertonstudents.com>
2024-01-29 09:04:22 -0800

    task 8

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    a_dictionary.pop(key, None)
+
+    if key in a_dictionary:
+        del a_dictionary[key]
 my_dictionary = {'a': 1, 'b': 2, 'c': 3}
 simple_delete(my_dictionary, 'b')
 simple_delete(my_dictionary, 'd')
-
 print(my_dictionary)
