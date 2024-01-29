5c9ea36d6d99d37182a25a2417ddb78b612c436a
IsmaelBuere <7674@holbertonstudents.com>
2024-01-29 08:46:48 -0800

    task 7

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    a_dictionary[key] = value
+    a_dictionary.update({key: value})
 my_dictionary = {'a': 1, 'b': 2, 'c': 3}
 update_dictionary(my_dictionary, 'b', 10)
 update_dictionary(my_dictionary, 'd', 4)
