d0d24f5af15dfb7011190fc94f277dcf9a97d2a3
IsmaelBuere <7674@holbertonstudents.com>
2024-01-29 09:07:10 -0800

    task 8

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -3,7 +3,4 @@ def simple_delete(a_dictionary, key=""):
 
     if key in a_dictionary:
         del a_dictionary[key]
-my_dictionary = {'a': 1, 'b': 2, 'c': 3}
-simple_delete(my_dictionary, 'b')
-simple_delete(my_dictionary, 'd')
-print(my_dictionary)
+return a_dictionary[key]
