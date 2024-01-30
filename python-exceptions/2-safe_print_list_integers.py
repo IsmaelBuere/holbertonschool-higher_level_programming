b3deee62bb35f58011cc6694c9671409a37d0e4e
IsmaelBuere <7674@holbertonstudents.com>
2024-01-30 21:58:17 +0000

    task 2

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count_integers = 0
+
+    try:
+        for i in range(x):
+            value = my_list[i]
+            print("{:d}".format(int(value)), end=' ')
+            count_integers += 1
+    except (ValueError, TypeError, IndexError):
+        pass
+    print()
+    return count_integers
