070289cab819013cd9adef5e19f7cf2df1964b08
IsmaelBuere <7674@holbertonstudents.com>
2024-01-30 22:43:26 +0000

    task 5

--- python-exceptions/5-raise_exception.py
+++ python-exceptions/5-raise_exception.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def raise_exception():
-    try:
-        raise ValueError("Custom type exception raised")
-    except ValueError as e:
-        print("Caught an exception:", e)
+    raise TypeError
