17d179401764409d255c7280397004ce30ce2796
IsmaelBuere <7674@holbertonstudents.com>
2024-01-31 05:48:56 -0800

    task 1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
+"""
+This is the module docstring. This module contains a definition of a Square.
+"""
+
+
 class Square:
     """
     A class that defines a square.
-    Attributes: - __size (int): Private instance attribute
+    Attributes:
+    - __size (int): Private instance attribute
     representing the size of the square.
     """
-
     def __init__(self, size):
         """
         Initializes an instance of the Square class with a given size.
