3ecc89dca1fd88f49298f4a011acb5607e9a0a9a
IsmaelBuere <7674@holbertonstudents.com>
2024-01-31 05:38:03 -0800

    task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+class Square:
+    """
+    A class that defines a square.
+
+    Attributes:
+    - __size (int): Private instance attribute
+    representing the size of the square.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes an instance of the Square class with a given size.
+
+        Parameters:
+        - size (int): The size of the square.
+        """
+        self.__size = size
