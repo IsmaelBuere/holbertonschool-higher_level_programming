9e7bd80442e5dda5ffcb4af09161ba5f2afb76ce
IsmaelBuere <7674@holbertonstudents.com>
2024-02-03 16:19:12 +0000

    task 2

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -8,16 +8,26 @@ def matrix_divided(m, div):
     """
     Divides all elements of a matrix.
     """
-    err = "matrix must be a matrix (list of lists) of integers/floats"
-    if not isinstance(m, list) or not all(isinstance(r, list) for r in m):
-        raise TypeError(err)
-    if not all(isinstance(i, (int, float)) for r in m for i in r):
-        raise TypeError(err)
-    if not all(len(r) == len(m[0]) for r in m):
-        raise TypeError("Each row of the matrix must have the same size")
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    return [[round(i / div, 2) for i in r] for r in m]
+    msg = "matrix must be a matrix (list of lists) of integers/floats"
+    if type(matrix) is not list or len(matrix) == 0 or len(matrix[0]) == 0:
+        raise TypeError(msg)
+
+    new_matrix = []
+    samelen = len(matrix[0])
+    for lists in matrix:
+        if type(lists) is not list:
+            raise TypeError(msg)
+        if len(lists) != samelen:
+            raise TypeError("Each row of the matrix must have the same size")
+        newlist = []
+        for i in lists:
+            if not isinstance(i, (int, float)):
+                raise TypeError(msg)
+            newlist.append(round(i/div, 2))
+        new_matrix.append(newlist)
+    return new_matrix
