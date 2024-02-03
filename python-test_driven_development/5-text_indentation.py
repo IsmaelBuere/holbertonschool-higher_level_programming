d976c08e0e07001b97ba998e911c8d87a23494e9
IsmaelBuere <7674@holbertonstudents.com>
2024-02-03 16:01:33 +0000

    task 5

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -6,7 +6,8 @@ Contains method that prints text with 2 new lines after each ".", "?", and ":"
 
 def text_indentation(text):
     """
-    Prints the provided text with additional line breaks after periods ('.'), question marks ('?'), and colons (':').
+    Prints the provided text with additional line breaks after
+    periods ('.'), question marks ('?'), and colons (':').
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
