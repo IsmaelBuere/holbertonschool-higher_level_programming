8cea322ea267d4216ff0fb8b39742574cc396446
IsmaelBuere <7674@holbertonstudents.com>
2024-02-09 14:39:00 +0000

    task 0

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Returns a list of the available attributes and methods.
+"""
+
+
+def lookup(obj):
+    """
+    This function returns a list of the attributes and methods of the provided object.
+    """
+    return dir(obj)
