a533ecac3be5c7b720d8b6dedf3433993c172066
IsmaelBuere <7476@holbertonstundets.com>
2024-02-10 16:27:24 +0000

    task 12

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Generate Pascal's triangle.
+"""
+
+
+def pascal_triangle(n):
+    """
+    Generates Pascal's triangle up to 'n' rows.
+    """
+    if n <= 0:
+        return []
+    
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        last_row = triangle[i-1]
+        for j in range(len(last_row)-1):
+            row.append(last_row[j] + last_row[j+1])
+        row.append(1)
+        triangle.append(row)
+    
+    return triangle
