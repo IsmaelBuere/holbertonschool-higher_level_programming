29666780de8d795b88e033700adf9b443ae85a0b
IsmaelBuere <7476@holbertonstundets.com>
2024-02-10 16:08:21 +0000

    task 8

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -6,7 +6,7 @@ Return the dictionary description for JSON serialization
 
 def class_to_json(obj):
     """
-    Returns the dictionary description with simple data 
+    Returns the dictionary description with simple data
     structure for JSON serialization of an object.
     """
     return obj.__dict__
