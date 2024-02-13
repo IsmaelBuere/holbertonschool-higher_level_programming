578656a0ba9e60f6ff8f71d9f436491733506741
IsmaelBuere <7476@holbertonstundets.com>
2024-02-13 17:22:25 +0000

    task 11

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,20 +1,57 @@
 #!/usr/bin/python3
-"""
-Defines a Square class.
-"""
+"""Defines a Square class."""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """
-    Represent a square.
-    """
+    """Represent a square."""
 
     def __init__(self, size, x=0, y=0, id=None):
         """
         Initialize a new Square.
         """
         super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """Get the size of the square."""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square."""
+        self.width = value
+        self.height = value
+
+    @property
+    def width(self):
+        """Get the width of the square."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set the width of the square."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+        self.__height = value
+
+    @property
+    def height(self):
+        """Get the height of the square."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set the height of the square."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+        self.__width = value
 
     def __str__(self):
         """Return the string representation of the square."""
