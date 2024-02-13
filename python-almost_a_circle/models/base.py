8f43e48a0720358ceec3604dd14f408b658f0925
IsmaelBuere <7476@holbertonstundets.com>
2024-02-13 17:41:38 +0000

    task 17

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -26,11 +26,11 @@ class Base:
         return json.dumps(list_dictionaries)
 
     @staticmethod
-    def to_json_string(list_dictionaries):
-        """Return the JSON string representation of list_dictionaries."""
-        if list_dictionaries is None or len(list_dictionaries) == 0:
-            return "[]"
-        return json.dumps(list_dictionaries)
+    def from_json_string(json_string):
+        """Return the list represented by json_string."""
+        if not json_string:
+            return []
+        return json.loads(json_string)
 
     @classmethod
     def save_to_file(cls, list_objs):
