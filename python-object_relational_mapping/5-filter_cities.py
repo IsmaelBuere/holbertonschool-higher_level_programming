0104d7cc13accec6eb9a96398a9b91fe4511f3a3
IsmaelBuere <7674@holbertonstudents.com>
2024-03-15 19:19:29 +0000

    task 5

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
 """
-Script that takes in the name of a state as an argument and lists all cities of
-that state, using the database hbtn_0e_4_usa. This version is safe from MySQL
-injections.
+Lists all cities from a specified state in the
+`hbtn_0e_4_usa` database, ordered by id.
 """
 
 import MySQLdb
-import sys
+from sys import argv
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name FROM cities \
-                JOIN states ON cities.state_id = states.id \
-                WHERE states.name = %s ORDER BY cities.id ASC", (sys.argv[4],))
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
-    db.close()
+if __name__ == '__main__':
+
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
+
+    cursor = db.cursor()
+    cursor.execute("""SELECT cities.name
+          FROM states
+          INNER JOIN cities ON states.id = cities.state_id
+          WHERE states.name = %s
+          ORDER BY cities.id ASC""", (state_name,))
+
+    print(", ".join([row[0] for row in cursor.fetchall()]))
