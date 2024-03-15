db82a318d07d0ce77f7801247c71154fc6238233
IsmaelBuere <7674@holbertonstudents.com>
2024-03-15 18:15:19 +0000

    task 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -13,6 +13,7 @@ if __name__ == '__main__':
 
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
-
-    for row in cursor.fetchall():
+    states = cur.fetchall()
+    
+    for row in states:
         print(row)
