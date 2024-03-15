133a35c603ccdfdc2529499b71c0e8b7f2ee3055
IsmaelBuere <7674@holbertonstudents.com>
2024-03-15 19:10:47 +0000

    task 6

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -6,14 +6,13 @@ Defines the class State and an instance Base using SQLAlchemy ORM.
 from sqlalchemy import Column, Integer, String, ForeignKey
 from model_state import Base
 
-Base = declarative_base()
 
 class State(Base):
     """
     The class definition of a State that inherits from Base.
     """
-
     __tablename__ = 'cities'
+
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
