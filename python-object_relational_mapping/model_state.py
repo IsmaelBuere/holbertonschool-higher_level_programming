59045ecb498632b192d8ba36fbee50abbbfad0a5
IsmaelBuere <7674@holbertonstundent.com>
2024-03-17 00:22:22 +0000

    tasks

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,21 +1,14 @@
-#!/usr/bin/python3
-"""
-This module contains a Base class.
-"""
-
-
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 
-class City(Base):
+class State(Base):
     """
-    City class inherits the Base class
+    State class inherits the Base class
     """
-    __tablename__ = 'cities'
+    __tablename__ = 'states'
 
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
