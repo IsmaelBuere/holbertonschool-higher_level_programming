56b0f986223beb53f3b05328f04e696f97fcb006
IsmaelBuere <7674@holbertonstudents.com>
2024-05-09 09:20:49 -0700

    task 8

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -2,4 +2,3 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 print(str[-76:-43] + str[-7:])
-print(str)
