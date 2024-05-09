07911557531de8ab86904bdc21ee6a6b23965557
IsmaelBuere <7674@holbertonstudents.com>
2024-05-09 09:19:53 -0700

    task 8

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str[::len(str)//len(str)])
+print(str[-76:-43] + str[-7:])
 print(str)
