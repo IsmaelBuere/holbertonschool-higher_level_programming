3b992cafc5a1cd6f86dff29fa693ef9f13507200
IsmaelBuere <7674@holbertonstudents.com>
2024-05-11 13:26:31 +0000

    task 3

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if chr(i) not in ['q', 'e']:
-        print(f"{chr(i)}", end='')
+for char in range(97, 123):
+    if char == 101 or char == 113:
+        continue
+    print("{:c}".format(char), end="")
