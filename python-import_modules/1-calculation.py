1d728b79bd68ca1d00ffa7031ee0ee46cb01cce2
IsmaelBuere <7674@holbertonstundets.com>
2024-05-12 14:31:53 +0000

    task 1

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -3,12 +3,13 @@ if __name__ == "__main__":
     import sys
 
     n = len(sys.argv)
-    if n == 2:
-        print(f"{n-1} argument:")
-        print(f"1: {sys.argv[1]}")
-    elif n == 1:
+    if n == 1:
         print("No arguments.")
     else:
-        print(f"{n-1} arguments:")
+        if n == 2:
+            print(f"{n-1} argument:")
+        else:
+            print(f"{n-1} arguments:")
+        
         for i in range(1, n):
             print(f"{i}: {sys.argv[i]}")
