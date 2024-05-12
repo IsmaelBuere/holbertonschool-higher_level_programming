bcadf2bc36e1ffc27f60eb6baba7a10f5fbdc85a
IsmaelBuere <7674@holbertonstundets.com>
2024-05-12 14:35:32 +0000

    task 1

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
+    from calculator_1 import add
+
+    a = 10
+    b = 5
 
     count = len(sys.argv) - 1
     if count == 0:
@@ -11,3 +15,6 @@ if __name__ == "__main__":
         print("{} arguments:".format(count))
     for i in range(count):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
+
+    print("Addition of a and b: ", add(a, b))
+
