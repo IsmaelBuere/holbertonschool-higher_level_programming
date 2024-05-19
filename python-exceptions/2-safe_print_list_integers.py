7090a316531593b0651386c63609164fec87c6c9
IsmaelBuere <7674@holbertonstudents.com>
2024-05-19 18:54:03 +0000

    task 2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -4,7 +4,7 @@ def safe_print_list_integers(my_list=[], x=0):
     count = 0
     for i in range(X):
         try:
-            print("{:d}".format(my_list[i], end="")
+            print("{:d}".format(my_list[i]), end="")
             value += 1
         except IndexError as e:
             raise IndexError(e)
