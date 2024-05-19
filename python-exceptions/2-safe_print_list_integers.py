2c310791fd2b7caf16f9ed79e6ee30d74240215d
IsmaelBuere <7674@holbertonstudents.com>
2024-05-19 18:59:07 +0000

    task 2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
-            value += 1
+            count += 1
         except IndexError as e:
             raise IndexError(e)
         except Exception:
