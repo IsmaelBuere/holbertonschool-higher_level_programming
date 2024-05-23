ebb364ef4931b2011ff5a89deccf3287ed65ce69
IsmaelBuere <7674@holbertonstundets.com>
2024-05-23 15:51:42 +0000

    task 0

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''
+Write a function that adds 2 integers
+'''
+def add_integer(a, b=98):
+    '''
+    Adds two integers
+    '''
+    TypeError - if a or b are not integers or floats
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    # Cast to integers if they are floats
+    a = int(a)
+    b = int(b)
+
+    return a + b
