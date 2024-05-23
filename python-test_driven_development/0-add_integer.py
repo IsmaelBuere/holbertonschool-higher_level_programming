2226887cea8874ccdef23073b5c5b1c8c3d994d2
IsmaelBuere <7674@holbertonstundets.com>
2024-05-23 16:04:00 +0000

    task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
-'''
+"""
 Write a function that adds 2 integers
-'''
-
-
+"""
 def add_integer(a, b=98):
-    '''
-    Adds two integers
-    '''
-    TypeError - if a or b are not integers or floats
+    """
+    Adds two numbers who should are integers.
+    """
+
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
 
-    # Cast to integers if they are floats
     a = int(a)
     b = int(b)
 
