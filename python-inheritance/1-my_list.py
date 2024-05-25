b380f60195fe1f2992d32782209e48e94ae08e1b
IsmaelBuere <7674@holbertonstudents.com>
2024-05-25 19:01:36 +0000

    task 1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -5,9 +5,9 @@ Write a class MyList that inherits from list
 
 
 class MyList(list):
-    def print_sorted(self):
         """
         Prints the list in ascending order.
         """
+    def print_sorted(self):
         sorted_list = sorted(self)
         print(sorted_list)
