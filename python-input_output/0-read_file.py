42ce2478e794779d07d30f58b3c7a15df4ff50d2
IsmaelBuere <7674@holbertonstudents.com>
2024-05-30 08:27:58 -0700

    task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,6 +5,9 @@ Write a function that reads a text file
 
 
 def read_file(filename=""):
+    """
+    Try to open a file
+    """
     try:
         with open(filename, "r", encoding="utf-8") as file:
             print(file.read())
