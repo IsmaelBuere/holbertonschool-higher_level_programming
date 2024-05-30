bf6db482879050976db510fd7f9989d0cc3e62d7
IsmaelBuere <7674@holbertonstudents.com>
2024-05-30 08:45:53 -0700

    task 4

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -2,3 +2,11 @@
 """
 Write a function that returns an object
 """
+import json
+
+
+def from_json_string(my_str):
+    """
+    Returns an object represented by a JSON string.
+    """
+    return json.loads(my_str)
