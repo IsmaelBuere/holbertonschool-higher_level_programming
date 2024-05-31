50f096b71722a6341a4668b2937b9c122299c1b8
IsmaelBuere <7674@holbertonstudents.com>
2024-05-31 18:58:49 +0000

    task 10

---
+++ python-input_output/11-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Write a class Student that defines a student
+"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if isinstance(attrs, list) and all(isinstance(attr, str) for attr in attrs):
+            return {key: value for key, value in self.__dict__.items() if key in attrs}
+        else:
+            return self.__dict__.copy()
+
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            setattr(self, key, value)
