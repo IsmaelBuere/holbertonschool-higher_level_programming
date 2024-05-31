b209c84219e45bc4c5d0a6d7bd6837d7f89395c3
IsmaelBuere <7674@holbertonstudents.com>
2024-05-31 18:19:55 +0000

    task 7

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+
+"""
+import os
+import sys
+from 5-save_to_json_file import save_to_json_file
+from 6-load_from_json_file import load_from_json_file
+
+filename = "add_item.json"
+
+
+if os.path.exists(filename):
+    items = load_from_json_file(filename)
+else:
+    items = []
+
+items.extend(sys.argv[1:])
+
+save_to_json_file(items, filename)
