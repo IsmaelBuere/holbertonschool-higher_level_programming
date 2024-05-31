104266cf5a90e48a48d2117ea04ca970773fbb26
IsmaelBuere <7674@holbertonstudents.com>
2024-05-31 18:08:56 +0000

    task 6

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -9,6 +9,5 @@ def load_from_json_file(filename):
     """
     Function : load_from_json_file is creating an object
     """
-    with open(filename, "r", encoding="utf-8") as fic:
-        r = json.load(fic)
-        return r
+    with open(filename, "r", encoding="UTF-8") as f:
+        return json.load(f)
