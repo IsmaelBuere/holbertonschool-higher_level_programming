9e0312c84dc4aec3a0fc14f43eac0e8081168511
IsmaelBuere <7674@holbertonstudents.com>
2024-06-22 13:00:24 -0700

    task 12

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,7 @@
 -- database dump from hbtn_0d_tvshows to your MySQL
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-LEFT JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+FROM tv_show_genres
+RIGHT JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_show_genres.show_id IS NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
