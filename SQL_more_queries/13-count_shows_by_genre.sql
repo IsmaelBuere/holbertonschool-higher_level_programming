68298ee0ba90e556f0ea8321448c345479c5906a
IsmaelBuere <7674@holbertonstudents.com>
2024-06-22 13:03:39 -0700

    task 13

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,6 @@
 -- database dump from hbtn_0d_tvshows
-SELECT tv_show_genres.genre_id AS genre,
-       COUNT(tv_shows.id) AS number_of_shows
-FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-GROUP BY tv_show_genres.genre_id
-HAVING number_of_shows > 0
+SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name
 ORDER BY number_of_shows DESC;
