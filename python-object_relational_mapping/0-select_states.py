66867282a3b291f9798f43212e7c4b9a7f964aaa
IsmaelBuere <7674@holbertonstudents.com>
2024-06-23 12:42:15 -0700

    task 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-that lists all states from the database
+Lists all states from the database.
 """
 import sys
 import MySQLdb
@@ -8,17 +8,21 @@ import MySQLdb
 
 def main():
     """
-    Connects to the MySQL
+    Connects to the MySQL server and lists all states.
     """
     if len(sys.argv) != 4:
-        print("Usage: ./0-select_states.py <mysql username> <mysql password> <database name>")
+        print("Usage: ./0-select_states.py <mysql username> <mysql password> "
+              "<database name>")
         return
 
     username = sys.argv[1]
     password = sys.argv[2]
     dbname = sys.argv[3]
 
-    db = MySQLdb.connect(host="localhost", user=username, passwd=password, db=dbname, port=3306)
+    db = MySQLdb.connect(
+        host="localhost", user=username, passwd=password, db=dbname,
+        port=3306
+    )
 
     cursor = db.cursor()
 
@@ -31,6 +35,7 @@ def main():
 
     cursor.close()
     db.close()
+
 
 if __name__ == "__main__":
     main()
