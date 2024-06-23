6eef518e923c5bb57740783f72e8a931eea505c8
IsmaelBuere <7674@holbertonstudents.com>
2024-06-23 12:46:07 -0700

    task 1

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+that lists all states with a name starting with N
+"""
+import sys
+import MySQLdb
+
+
+def main():
+    """
+    Connects to the MySQL server and lists all states starting with 'N'.
+    """
+    if len(sys.argv) != 4:
+        print("Usage: ./1-filter_states.py <mysql username> <mysql password> "
+              "<database name>")
+        return
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbname = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host="localhost", user=username, passwd=password, db=dbname,
+        port=3306
+    )
+
+    cursor = db.cursor()
+
+    query = "SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
+    cursor.execute(query)
+
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
