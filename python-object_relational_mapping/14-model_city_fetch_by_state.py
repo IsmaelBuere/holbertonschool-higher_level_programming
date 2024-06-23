0865c8df36ba0c0fa7d9b36d0c66460416a48887
IsmaelBuere <7674@holbertonstudents.com>
2024-06-23 14:55:46 -0700

    fix 14?

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -13,12 +13,11 @@ if __name__ == "__main__":
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
 
     Session = sessionmaker(bind=engine)
-
     session = Session()
 
-    cities = session.query(City).join(State).order_by(City.id).all()
+    query = session.query(City, State). \
+        join(State, City.state_id == State.id). \
+        order_by(City.id).all()
 
-    for city in cities:
-        print("{}: ({}) {}".format(city.state.name, city.id, city.name))
-
-    session.close()
+    for row in query:
+        print("{}: ({}) {}".format(row.State.name, row.City.id, row.City.name))
