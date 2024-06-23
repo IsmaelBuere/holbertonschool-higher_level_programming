920a9ecf9b03ae38173c822c653d8284eb6d40f8
IsmaelBuere <7674@holbertonstudents.com>
2024-06-23 12:33:08 -0700

    task 0

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+that lists all states from the database
+"""
+
+
+import MySQLdb
+
+def get_states(username, password, database):
+    try:
+        db_connection = MySQLdb.connect(host='localhost', user=username, passwd=password, db=database)
+    except MySQLdb.Error as e:
+        print(f"Error connecting to the database: {e}")
+        return
+
+    cursor = db_connection.cursor()
+
+    query = "SELECT id, name FROM states ORDER BY id"
+    cursor.execute(query)
+
+    for row in cursor.fetchall():
+        state_id, state_name = row
+        print(f"({state_id}, '{state_name}')")
+
+    db_connection.close()
+
+if __name__ == "__main__":
+    username = input("Enter MySQL username: ")
+    password = input("Enter MySQL password: ")
+    database = input("Enter database name: ")
+
+    get_states(username, password, database)
