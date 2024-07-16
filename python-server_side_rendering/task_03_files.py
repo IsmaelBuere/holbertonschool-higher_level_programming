02e08937a99b2575c3f8410df11e295ff840c9f0
IsmaelBuere <7674@holbertonstudents.com>
2024-07-16 09:55:13 -0700

    task 3

---
+++ python-server_side_rendering/task_03_files.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template, request
+import csv
+import json
+
+app = Flask(__name__)
+
+# Function to read JSON data from file
+def read_json_file(filename):
+    with open(filename, 'r') as file:
+        data = json.load(file)
+    return data
+
+# Function to read CSV data from file
+def read_csv_file(filename):
+    data = []
+    with open(filename, 'r') as file:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
+            data.append(row)
+    return data
+
+# Route to handle displaying products
+@app.route('/products')
+def display_products():
+    source = request.args.get('source')
+    id_param = request.args.get('id')
+    products = []
+    error = None
+
+    if source not in ['json', 'csv']:
+        error = "Wrong source"
+    else:
+        try:
+            if source == 'json':
+                products_data = read_json_file('products.json')
+            elif source == 'csv':
+                products_data = read_csv_file('products.csv')
+            
+            if id_param:
+                products = [product for product in products_data if str(product['id']) == id_param]
+                if not products:
+                    error = "Product not found"
+            else:
+                products = products_data
+
+        except FileNotFoundError:
+            error = f"File 'products.{source}' not found"
+
+    return render_template('product_display.html', products=products, error=error)
+
+if __name__ == '__main__':
+    app.run(debug=True)
