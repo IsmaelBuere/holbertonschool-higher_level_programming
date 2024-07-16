24c01a3511a9d7fde2edbb8f59f29e690b334a28
IsmaelBuere <7674@holbertonstudents.com>
2024-07-16 09:27:35 -0700

    task 2

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -1,5 +1,7 @@
+#!/usr/bin/python3
 from flask import Flask, render_template
 import json
+import os
 
 app = Flask(__name__)
 
@@ -17,9 +19,18 @@ def contact():
 
 @app.route('/items')
 def items():
-    with open('items.json') as f:
-        data = json.load(f)
-    return render_template('items.html', items=data['items'])
+    items_list = []
+    try:
+        if os.path.exists('items.json'):
+            with open('items.json') as f:
+                data = json.load(f)
+                items_list = data.get('items', [])
+        else:
+            print("File 'items.json' not found.")
+    except Exception as e:
+        print(f"Error reading 'items.json': {e}")
+    
+    return render_template('items.html', items=items_list)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
