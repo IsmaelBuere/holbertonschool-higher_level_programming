acca858daa957ae0c4a4fca7ca6299ebfc62dc8c
IsmaelBuere <7674@holbertonstudents.com>
2024-07-28 10:55:40 -0700

    task 1

--- javascript-dom_manipulation/1-script.js
+++ javascript-dom_manipulation/1-script.js
@@ -1,5 +1,9 @@
-const redHeader = document.querySelector('#red_header');
-redHeader.addEventListener('click', () => {
-  const header = document.querySelector('header');
-  header.style.color = '#FF0000';
+document.addEventListener("DOMContentLoaded", function() { 
+
+    const redHeaderElement = document.querySelector('#red_header');
+    const headerElement = document.querySelector('header');
+        
+    redHeaderElement.addEventListener('click', function() {        
+        headerElement.style.color = '#FF0000';
+    });
 });
