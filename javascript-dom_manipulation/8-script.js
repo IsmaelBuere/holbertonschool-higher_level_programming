2a55e5519762f5e16e25891ec7099d261c86bfb0
IsmaelBuere <7674@holbertonstudents.com>
2024-07-28 11:06:06 -0700

    task 8

---
+++ javascript-dom_manipulation/8-script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('https://hellosalut.stefanbohacek.dev/?lang=fr')
+      .then(response => response.json())
+      .then(data => {
+        const helloElement = document.querySelector('#hello');
+        helloElement.textContent = data.hello;
+      })
+      .catch(error => console.error('Error fetching data:', error));
+  });
+  
